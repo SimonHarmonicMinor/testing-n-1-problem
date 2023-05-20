92864a7e03b480919df2eb0e973d1a213d5543f9
Семен Киреков <sakireko@mts.ru>
2023-05-20 22:42:40 +0300

    add constructors to ZooResponse that accepts Zoo as a parameter

--- src/main/java/com/example/demo/dto/ZooResponse.java
+++ src/main/java/com/example/demo/dto/ZooResponse.java
@@ -1,5 +1,8 @@
 package com.example.demo.dto;
 
+import com.example.demo.entity.Animal;
+import com.example.demo.entity.Zoo;
+
 import java.util.List;
 
 public record ZooResponse(
@@ -7,6 +10,13 @@ public record ZooResponse(
     String name,
     List<AnimalResponse> animals
 ) {
+    public ZooResponse(Zoo zoo) {
+        this(zoo.getId(), zoo.getName(), zoo.getAnimals().stream().map(AnimalResponse::new).toList());
+    }
+
     public record AnimalResponse(Long id, String name) {
+        public AnimalResponse(Animal animal) {
+            this(animal.getId(), animal.getName());
+        }
     }
 }
