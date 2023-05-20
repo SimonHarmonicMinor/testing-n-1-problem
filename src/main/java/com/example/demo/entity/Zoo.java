3bada921a76e3c3357c632cc205153002bd03cfe
Семен Киреков <sakireko@mts.ru>
2023-05-20 22:30:26 +0300

    add Zoo.animals persist + Zoo.addAnimal

--- src/main/java/com/example/demo/entity/Zoo.java
+++ src/main/java/com/example/demo/entity/Zoo.java
@@ -3,6 +3,7 @@ package com.example.demo.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -11,6 +12,7 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import static jakarta.persistence.CascadeType.PERSIST;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -24,6 +26,13 @@ public class Zoo {
 
     private String name;
 
-    @OneToMany(mappedBy = "zoo")
+    @OneToMany(mappedBy = "zoo", cascade = PERSIST)
     private List<Animal> animals = new ArrayList<>();
+
+    public void addAnimal(String name) {
+        final var animal = new Animal();
+        animal.setName(name);
+        animal.setZoo(this);
+        animals.add(animal);
+    }
 }
