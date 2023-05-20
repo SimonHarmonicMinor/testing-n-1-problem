9972bc2083404722d307e5d81dd17924586f10c0
Семен Киреков <sakireko@mts.ru>
2023-05-20 22:10:12 +0300

    move Zoo and Animal entities to .entity package

--- src/main/java/com/example/demo/entity/Animal.java
+++ src/main/java/com/example/demo/entity/Animal.java
@@ -1,29 +1,28 @@
-package com.example.demo;
-
-import java.util.ArrayList;
-import java.util.List;
+package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
-@Table(name = "zoo")
+@Table(name = "animal")
 @Getter
 @Setter
-public class Zoo {
+public class Animal {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "zoo_id")
+    private Zoo zoo;
 
     private String name;
-
-    @OneToMany(mappedBy = "zoo")
-    private List<Animal> animals = new ArrayList<>();
 }
--- src/main/java/com/example/demo/entity/Zoo.java
+++ src/main/java/com/example/demo/entity/Zoo.java
@@ -1,28 +1,29 @@
-package com.example.demo;
+package com.example.demo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
-@Table(name = "animal")
+@Table(name = "zoo")
 @Getter
 @Setter
-public class Animal {
+public class Zoo {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "zoo_id")
-    private Zoo zoo;
 
     private String name;
+
+    @OneToMany(mappedBy = "zoo")
+    private List<Animal> animals = new ArrayList<>();
 }
