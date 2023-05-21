2d97f73789f1381602172e033155e4628e962923
Семен Киреков <sakireko@mts.ru>
2023-05-21 13:12:36 +0300

    add ZooRepository.findAllWithAnimalsJoined

--- src/main/java/com/example/demo/repository/ZooRepository.java
+++ src/main/java/com/example/demo/repository/ZooRepository.java
@@ -3,6 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.entity.Zoo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ZooRepository extends JpaRepository<Zoo, Long> {
+    @Query("FROM Zoo z LEFT JOIN FETCH z.animals")
+    List<Zoo> findAllWithAnimalsJoined();
 }
